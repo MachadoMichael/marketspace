1b5c750188fcbb8d58e05f3c0239563356330d37
Michael Machado <machado.m3@icloud.com>
2023-02-02 10:48:37 -0300

    remove some console.log

--- src/services/product/removeImage.ts
+++ src/services/product/removeImage.ts
@@ -3,11 +3,8 @@ import { api } from "../api";
 
 export const removeImage = async (images: any) => {
   try {
-    // const reqImage = [image.id] as
-    console.log("PREIAMGES", images);
-    const deletedImage = await api.delete("/products/images", images);
+    await api.delete("/products/images", images);
 
-    console.log("xxx", deletedImage);
     return true;
   } catch (error) {
     if (axios.isAxiosError(error))
